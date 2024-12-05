d7d149f0452d78739a848c4b00a031eadea79636
Kyle Glover <contact@kyle.rocks>
2024-12-05 14:14:18 +1100

    Refactored day 1 a little

--- src/day1/Part1.java
+++ src/day1/Part1.java
@@ -1,55 +1,59 @@
 package day1;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 import core.Result;
 
 public class Part1 {
-  /**
-   * Answer: 2166959
-   */
+  /** Answer: 2166959 */
   public static void main(String[] args) {
-    Result<String, IOException> result = readFile("src/day1/input.txt");
-    if (result.isError()) {
-      return;
+    final int[][] data = parseFile("src/day1/input.txt");
+
+    System.out.println(Arrays.toString(data[0]));
+    
+    int[] left = new int[data.length];
+    int[] right = new int[data.length];
+    for (int i = 0; i < data.length; i++) {
+      left[i] = data[i][0];
+      right[i] = data[i][1];
     }
 
-    String[] lines = result.getSuccess().trim().split("\n");
-    int[] left = new int[lines.length];
-    int[] right = new int[lines.length];
-    for (int i = 0; i < lines.length; i++) {
-      String[] parts = lines[i].split("\s+");
-      left[i] = Integer.parseInt(parts[0]);
-      right[i] = Integer.parseInt(parts[1]);
-    }
-    
     Arrays.sort(left);
     Arrays.sort(right);
     
     int total = 0;
-    for (int i = 0; i < lines.length; i++) {
+    for (int i = 0; i < data.length; i++) {
       total += Math.abs(left[i] - right[i]);
     }
+
     System.out.println(total);
   }
 
-  /**
-   * Read complete file contents.
-   * @param path String Relative path to the file
-   * @return String Unformatted file content
-   * @throws IOException
-   */
-  public static Result<String, IOException> readFile(String path) {
-    try {
-      String content = Files.readString(Path.of(path));
-      return Result.success(content);
+  /** Read complete file contents. */
+  public static Result<int[][], IOException> readFile(String path) {
+    try (Stream<String> lines = Files.lines(Path.of(path))) {
+      return Result.success(lines.map(x -> Arrays.stream(x.split("\\s+"))
+          .mapToInt(Integer::parseInt)
+          .toArray())
+        .toArray(int[][]::new));
     } catch (IOException error) {
-      System.out.println("An error occurred.");
-      error.printStackTrace();
       return Result.error(error);
     }
+  }
+
+  /** Parse file contents and format into usable data. */
+  public static int[][] parseFile(String filePath) {
+    final Result<int[][], IOException> result = readFile(filePath);
+    if (result.isError()) {
+      System.out.println("An error occurred while reading the file.");
+      return new int[0][0];
+    }
+    return result.getSuccess();
   }
 }
--- src/day1/Part2.java
+++ src/day1/Part2.java
@@ -3,50 +3,49 @@ package day1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import core.Result;
 
 public class Part2 {
-  /**
-   * Answer: 23741109
-   */
+  /** Answer: 23741109 */
   public static void main(String[] args) {
-    Result<String, IOException> result = readFile("src/day1/input.txt");
-    if (result.isError()) {
-      return;
-    }
+    final int[][] data = parseFile("src/day1/input.txt");
 
-    String[] lines = result.getSuccess().trim().split("\n");
-    int[] left = new int[lines.length];
     HashMap<Integer, Integer> right = new HashMap<>();
-    for (int i = 0; i < lines.length; i++) {
-      String[] parts = lines[i].split("\s+");
-      left[i] = Integer.parseInt(parts[0]);
-      right.merge(Integer.parseInt(parts[1]), 1, Integer::sum);
+    for (int[] line : data) {
+      right.merge(line[1], 1, Integer::sum);
     }
     
     int total = 0;
-    for (int n : left) {
-      total += n * right.getOrDefault(n, 0);
+    for (int[] line : data) {
+      total += line[0] * right.getOrDefault(line[0], 0);
     }
+
     System.out.println(total);
   }
 
-  /**
-   * Read complete file contents.
-   * @param path String Relative path to the file
-   * @return String Unformatted file content
-   * @throws IOException
-   */
-  public static Result<String, IOException> readFile(String path) {
-    try {
-      String content = Files.readString(Path.of(path));
-      return Result.success(content);
+  /** Read complete file contents. */
+  public static Result<int[][], IOException> readFile(String path) {
+    try (Stream<String> lines = Files.lines(Path.of(path))) {
+      return Result.success(lines.map(x -> Arrays.stream(x.split("\\s+"))
+          .mapToInt(Integer::parseInt)
+          .toArray())
+        .toArray(int[][]::new));
     } catch (IOException error) {
-      System.out.println("An error occurred.");
-      error.printStackTrace();
       return Result.error(error);
     }
+  }
+
+  /** Parse file contents and format into usable data. */
+  public static int[][] parseFile(String filePath) {
+    final Result<int[][], IOException> result = readFile(filePath);
+    if (result.isError()) {
+      System.out.println("An error occurred while reading the file.");
+      return new int[0][0];
+    }
+    return result.getSuccess();
   }
 }
